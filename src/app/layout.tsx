92d9e0901acba34ca38069792bb19d70cf5841f0
wotfrosty-1627 <wot.frosty@gmail.com>
2026-04-25 06:34:38 +0000

    revert: remove font settings from PROD - settings only in LAB branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/lib/theme-context";
-import { SettingsInitializer } from "@/components/SettingsInitializer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         <ThemeProvider>
-          <SettingsInitializer />
           {children}
         </ThemeProvider>
         <Toaster />
